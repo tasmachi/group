0089284c0d96af1c156a92b53b1047f19e7508a3
Tasmachi <ishamsiyev094@gmail.com>
2024-12-26 01:49:30 +0500

    add requirements.txt

--- NASA/settings.py
+++ NASA/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIR=os.path.join(BASE_DIR,'static')
 
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
